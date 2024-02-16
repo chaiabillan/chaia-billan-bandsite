17140a5e5befa8c79669efb5c1e0a73281b1c091
Chaia Billan <chaia_billan@yahoo.com>
2024-02-16 13:54:07 -0800

    fixed the band site api so that comments from api would display properly

--- scripts.js/index-page.js
+++ scripts.js/index-page.js
@@ -1,30 +1,21 @@
-const postsList = [
-    {
-        name: "Victor Pinto",
-        date: "11/02/2023",
-        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-    },
-    {
-        name: "Christina Cabrera",
-        date: "10/28/2023",
-        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-    },
-    {
-        name: "Isaac Tadesse",
-        date: "10/20/2023",
-        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-    }
-];
+// const postsList = [
+//     {
+//         name: "Victor Pinto",
+//         date: "11/02/2023",
+//         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+//     },
+//     {
+//         name: "Christina Cabrera",
+//         date: "10/28/2023",
+//         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+//     },
+//     {
+//         name: "Isaac Tadesse",
+//         date: "10/20/2023",
+//         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+//     }
+// ];
 
-//entire comment section 
-const joinConvo = document.querySelector(".join-convo");
-
-// this holds the previously posted(default) comments 
-const commentsPosted = document.createElement("div");
-commentsPosted.classList.add("comment__posted");
-joinConvo.appendChild(commentsPosted);
-
-//this function 
 function showAllComments(postsList) {
 
     commentsPosted.innerHTML = "";
@@ -36,7 +27,35 @@ function showAllComments(postsList) {
     }    
 }
 
-showAllComments(postsList);
+const renderComments = async () => {
+    try {
+        const result = await apiClient.getComments();
+        console.log(result);
+        return showAllComments(result);
+    } catch(errors) {
+        console.error(errors);
+    }
+    
+}
+renderComments();
+
+
+
+
+//entire comment section 
+const joinConvo = document.querySelector(".join-convo");
+
+// this holds the previously posted(default) comments 
+const commentsPosted = document.createElement("div");
+commentsPosted.classList.add("comment__posted");
+joinConvo.appendChild(commentsPosted);
+
+
+
+//this function 
+
+
+// showAllComments(postsList);
 
 function displayPostInfo(postList) {
 
